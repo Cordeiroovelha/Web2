adbd64e1cfb9f576bbd59ed2fc875c272bb9f31e
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-02-24 20:33:07 -0300

    Add files via upload

---
+++ EntradaDeDados.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EntradadeDados {
+    public static void main(String[] args){
+        // instanciar um obj da classe Scanner
+		Scanner teclado = new Scanner(System.in);
+        int num1, num2, r;
+        System.out.print("Numero 1 = ");
+        num1 = teclado.nextInt();
+        System.out.print("Numero 2 = ");
+        num2 = teclado.nextInt();
+        
+        r = num1 + num2;
+        
+        System.out.print("Soma = " + r);
+        
+        //fecha o teclado boa pratica
+        teclado.close();
+    }
+}
