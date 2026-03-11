ea8c12725413659083ff5cb9662da11822f7b859
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-11 20:35:04 -0300

    Refactor PeopleControlMain with additional features

--- OrientacaoAoObjeto/PeopleControlMain.java
+++ OrientacaoAoObjeto/PeopleControlMain.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class PeopleControlMain {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static PeopleManager manager = new PeopleManager();
 
     public static int menu(){
         System.out.println("---------------------------");
@@ -13,44 +14,94 @@ public class PeopleControlMain {
         System.out.println("------- Option Menu -------");
         System.out.println("1 - Sign In");
         System.out.println("2 - Search Id");
-        System.out.println("3 - Refresh");
+        System.out.println("3 - Update Name");
         System.out.println("4 - Remove");
         System.out.println("5 - Exist ");
         return scanner.nextInt();
     }
 
-    public static Person readData(){
+    public static void register(){
         System.out.println("Type the name: ");
         String name = scanner.nextLine();
         System.out.println("Type the Id: ");
-        String Id = scanner.nextLine();
-        return new Person(name, Id);
+        String id = scanner.nextLine();
+        Person person = new Person(name, id);
+        if(manager.save(person)){
+            System.out.println("Sign In complete");
+            System.out.println(manager.getPeopleList());
+        }
+        else
+            System.out.println("Error in Sign In");
+    }
+
+    private static void search() {
+        System.out.println("Type de Id to search: ");
+        String id = scanner.nextLine();
+        Person person = manager.search(id);
+        if(person != null){
+            System.out.println("Person find:");
+            System.out.println(person);
+        }else
+            System.out.println("Person not found, try other");
+    }
+
+    private static void update() {
+        System.out.println("What's the Id:");
+        String id = scanner.nextLine();
+        System.out.println("What's the new Name: ");
+        String newName = scanner.nextLine();
+        if(manager.update(id, newName)){
+            System.out.println("Data update");
+        }else
+            System.out.println("Error in update");
+    }
+
+    private static void remove() {
+        System.out.println("What's the Id: ");
+        String id = scanner.nextLine();
+        if(manager.remove(id)){
+            System.out.println("Person remove");
+        }else
+            System.out.println("Person not found");
     }
 
     public static void main(String[] args) {
-        PeopleManager manager = new PeopleManager();
-        Person person;
         int option;
         
         do {
             option = menu();
-            scanner.nextLine();
+            scanner.nextLine(); // limpesa de buffer
 
             switch (option) {
                 case 1: {
-                    person = readData();
-                    if(manager.save(person)){
-                        System.out.println("Sign In complete");
-                        System.out.println(manager.getPeopleList());
-                    }
-                    else
-                        System.out.println("Error in Sign In");
+                    register();
                     break;
                 }
-                default:
+                case 2: {
+                    search();
                     break;
+                }
+                case 3: {
+                    update();
+                    break;
+                }
+                case 4: {
+                    remove();
+                    break;
+                }
+                case 5: {
+                    System.out.println("Bye Bye, see you next time!");
+                    break;
+                }
+                default : {
+                System.out.println("Opção invalida, tente novamente"); 
+                break;
+                }
             }
-
+            System.out.println("Press <enter> to continue");
+            scanner.nextLine();
         } while (option != 5);
     }
+
+        
 }
