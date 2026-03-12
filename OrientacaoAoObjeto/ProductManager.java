2ed17d0d10cc3a58750aecaac8496cac52067116
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-11 22:21:24 -0300

    Update ProductManager.java

--- OrientacaoAoObjeto/ProductManager.java
+++ OrientacaoAoObjeto/ProductManager.java
@@ -10,9 +10,51 @@ public class ProductManager {
         productList = new ArrayList<>();
     }
 
-    public List<Product> getProductsList(){
+    public List<Product> getProductList(){
         return productList;
     }
 
-    
+    public boolean saveProduct(Product product){
+        if(product != null){
+            productList.add(product);
+            return true;
+        }
+        return false;
+    }
+
+    public Product search(String id){
+        for(Product product: productList){
+            if(product.getId().equals(id)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    // não sei se é a forma mais otimizada criar duas funçoes
+    public boolean updateName(String id, String newName){
+        Product product = search(id);
+        if(product != null){
+            int index = productList.indexOf(product);
+            if(index != -1){
+                product.setName(newName);
+                productList.set(index, product);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updateValue(String id, double newValue){
+        Product product = search(id);
+        if(product != null){
+            int index = productList.indexOf(product);
+            if(index != -1){
+                product.setValue(newValue);
+                productList.set(index, product);
+                return true;
+            }
+        }
+        return false;
+    }
 }
